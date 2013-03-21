ea4d7e4026826bb74afa799e90f4d81ae017a6ad
Tristan McNab <tristan@seditious-tech.com>
2013-03-20 20:19:39 -0400

    Initial commit

---
+++ Reader.Services/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using QDFeedParser;
+
+namespace Reader.Services
+{
+    class Test
+    {
+        void x()
+        {
+            var uri = new Uri("http://www.kungfugrippe.com/rss", UriKind.Absolute);
+            var ff = new HttpFeedFactory();
+            if (ff.PingFeed(uri))
+            {
+                var feed = ff.CreateFeed(uri);
+                
+            }
+        }
+    }
+}
---
+++ Reader.Web/Controllers/MainController.cs
@@ -0,0 +1,47 @@
+﻿namespace Reader.Web.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+    using NBrowserID;
+    using System.Web.Security;
+    using AttributeRouting.Web.Mvc;
+
+    public class MainController : Controller
+    {
+        
+        public ActionResult Index ()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            else
+            {
+                return View("Splash");
+            }
+        }
+
+        
+        [POST("/sign-in")]
+        public ActionResult SignIn(string assertion)
+        {
+            var authentication = new BrowserIDAuthentication();
+            var verificationResult = authentication.Verify(assertion);
+            if (verificationResult.IsVerified)
+            {
+                string email = verificationResult.Email;
+                FormsAuthentication.SetAuthCookie(email, false);
+                return Json(new { email });
+            }
+
+            return Json(null);
+        }
+
+        [GET("/sign-out")]
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/");
+        }
+    }
+}
