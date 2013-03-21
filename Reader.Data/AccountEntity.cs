6eeb39f10aeef42b51bd116b53bfad2b27018d0f
Tristan McNab <tristan@seditious-tech.com>
2013-03-20 20:47:28 -0400

    Data stuff.
    This was mostly just to trigger a build on AppHarbor.

---
+++ Reader.Data/AccountEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Reader.Data
+{
+    using System;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public sealed class AccountEntity : EntityBase
+    {
+        public decimal balance { get; set; }
+
+        public string email_address { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime last_login { get; set; }
+    }
+}
---
+++ Reader.Data/EntityBase.cs
@@ -0,0 +1,25 @@
+﻿namespace Reader.Data
+{
+    using System;
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using MongoDB.Bson.Serialization.IdGenerators;
+
+    /// <summary>
+    /// Base class for all objects stored in the database.
+    /// </summary>
+    public abstract class EntityBase
+    {
+        [BsonId(IdGenerator = typeof(StringObjectIdGenerator))]
+        public string id { get; set; }
+
+        [BsonIgnore]
+        public DateTime created
+        {
+            get
+            {
+                return ObjectId.Parse(this.id).CreationTime;
+            }
+        }
+    }
+}
