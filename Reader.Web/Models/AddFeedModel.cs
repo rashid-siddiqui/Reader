4128ae83db370ddd40118196a10098afb77d2a56
Tristan McNab <tristan@seditious-tech.com>
2013-03-21 16:05:55 -0400

    More UI Stuff

---
+++ Reader.Web/Models/AddFeedModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Reader.Web.Models
+{
+    public class AddFeedModel
+    {
+        [Required]
+        public Uri FeedUrl { get; set; }
+    }
+}
---
+++ Reader.Web/a/js/local/app.addfeed.js
@@ -0,0 +1,16 @@
+﻿
+
+App.AddFeed = (function (self, $) {
+    
+    var formSelector = '#AddFeed';
+
+    $(formSelector).on('submit', function (e) {
+        e.preventDefault();
+
+        
+
+    });
+
+
+    return self;
+}(App.AddFeed || {}, jQuery));
