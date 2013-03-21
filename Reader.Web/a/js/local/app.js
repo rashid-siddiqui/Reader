9d08e2172455ee34fca84c657b42e2f709d0aa1f
Tristan McNab <tristan@seditious-tech.com>
2013-03-21 15:08:01 -0400

    Try clear the AppHarbor build which wont deploy.

---
+++ Reader.Web/a/js/local/app.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="../lib/jquery-vsdoc.js" />
+/// <reference path="../lib/meny.js" />
+
+var App = (function (self, $, Meny)
+{
+    var meny = Meny.create({
+        menuElement: document.querySelector('.meny'),
+        contentsElement: document.querySelector('.container-fluid'),
+        position: 'left',
+        width: 300
+    });
+
+    document.ontouchmove = function (e) {
+        e.preventDefault();
+    };
+
+
+    
+
+
+    self.UI = self.UI || {
+
+
+
+    };
+
+
+
+
+    return self;
+}(App || {}, jQuery, Meny));
